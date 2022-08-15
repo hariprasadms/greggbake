25298056c9d693c260b13db21d12a6b8189c24de
Hariprasad <iamharims@gmail.com>
2022-08-15 13:14:32 +0100

    added ceil for total price.

--- lib/framework_code/custom_functions.dart
+++ lib/framework_code/custom_functions.dart
@@ -3,6 +3,7 @@ String count(List<dynamic> itemJson) {
   double totalprice = 0.0;
   for (int i = 0; i < itemJson.length; i++) {
     totalprice = totalprice + itemJson[i]['eatInPrice'];
+    totalprice.ceil();
   }
   return totalprice.toString();
 }
